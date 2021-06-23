8c24209b872e2ca8cb310cfb02f9273558abf011
ManonHofman <84331614+ManonHofman@users.noreply.github.com>
2021-06-23 16:28:40 +0200

    Add files via upload

---
+++ even-odd-reporter.js
@@ -0,0 +1,9 @@
+var number = 1;
+
+while (number = even) {
+console.log ("dit is een even getal")
+
+while (number = uneven)
+console.log ("dit is een oneven getal")
+number++
+}
---
+++ multiplication-tables.js
@@ -0,0 +1,3 @@
+for (Table = 9; number = 5; number x table; number ++){
+    console.log ()
+}
